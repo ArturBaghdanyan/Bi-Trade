8f2fcd0f22e14c3b74fa48f1334235c815b05316
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2024-09-08 11:00:37 +0400

    changes header container for about and contact pages

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,16 +1,23 @@
 import styles from "./styles.module.scss";
 import biTrade from "../../assets/header_icons/bi-trade.svg";
+import biTradeBlack from "../../assets/header_icons/bi-trade-black.svg";
+import uk from "../../assets/header_icons/uk.png";
+
 import { Link } from "react-router-dom";
 
 const Header = ({ isHomePage = false }) => {
 
-	console.log(isHomePage)
 	return (    
 		<header>
 			<div className={`${isHomePage ? styles.header : styles.header_about}`}>
 
 			<div className={styles.header_logo}>
-				<img src={biTrade} alt="bi-trade"/>
+				{/* When click to another page img will be changed */}
+				{isHomePage ? (
+					<img src={biTrade} alt="bi-trade"/>
+				) : (
+					<img src={biTradeBlack} alt="bi-trade-black"/>
+				)}
 			</div>
 			<div className={styles.header_nav}>
 				<nav>
@@ -29,6 +36,9 @@ const Header = ({ isHomePage = false }) => {
 					</select>
 					<Link to="/register" target="_blank">Registration</Link>
 				</nav>
+				<div className={styles.flag}>
+					<img src={uk} alt="uk"/>
+				</div>
 				<div className={styles.header_button}>
 					<button>Join Now</button>
 				</div>
